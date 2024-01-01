c7e4ecd1d4b7f38c371f86700f35c5bfb8c443c0
Nelson Obuya Ouya <nelsonobuya19@gmail.com>
2024-01-01 21:49:49 +0300

    Made the Nav responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,57 +1,97 @@
-
-import Logo from '../../assets/logo.png'
+import { useState } from 'react';
+import Logo from '../../assets/logo.png';
+import { IoIosMenu, IoMdClose } from 'react-icons/io';
 
 const NavLinks = [
-    {
-        id:1,
-        tittle: "Home",
-        link: "/#"
-    },
-     {
-        id:2,
-        tittle: "About",
-        link: "/#about"
-    },
-     {
-        id:3,
-        tittle: "Menu",
-        link: "/#menu"
-    },
-     {
-        id:4,
-        tittle: "Services",
-        link: "/#services"
-    },
-]
+  {
+    id: 1,
+    title: 'Home',
+    link: '/#',
+  },
+  {
+    id: 2,
+    title: 'About',
+    link: '/#about',
+  },
+  {
+    id: 3,
+    title: 'Menu',
+    link: '/#menu',
+  },
+  {
+    id: 4,
+    title: 'Services',
+    link: '/#services',
+  },
+];
+
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+
+  const handleLinkClick = () => {
+    setMenu(false);
+  };
+
   return (
     <div className='shadow-xl'>
-        <div className="container md:py-3 sm:py-0 ">
-            <div className="flex items-center justify-between ">
-                {/* LOGO */}
-            
-                <div className="">
-                    <a href="/#">
-                        <img src={Logo} alt="" className='w-16'/>
+      <div className='container md:py-3 sm:py-0 '>
+        <div className='flex items-center justify-between '>
+          <div className=''>
+            <a href='/#'>
+              <img src={Logo} alt='' className='w-16' />
+            </a>
+          </div>
+          <div className='flex items-center justify-between gap-4'>
+            <ul className='items-center hidden gap-4 md:flex'>
+              {NavLinks.map(({ id, title, link }) => (
+                <li key={id}>
+                  <a
+                    className='px-4 py-4 duration-300 hover:text-primary'
+                    href={link}
+                    onClick={handleLinkClick} // Close menu when link is clicked
+                  >
+                    {title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+            {menu && (
+              <ul className='items-center gap-4 flex nav-links md:hidden mx-auto'>
+                {NavLinks.map(({ id, title, link }) => (
+                  <li key={id}>
+                    <a
+                      className='px-4 py-4 duration-300 hover:text-primary'
+                      href={link}
+                      onClick={handleLinkClick} // Close menu when link is clicked
+                    >
+                      {title}
                     </a>
-                </div>
-                {/* NAVLINKS */}
-                <div className="flex items-center justify-between gap-4">
-                    <ul className='items-center hidden gap-4 sm:flex'>
-                        {
-                            NavLinks.map(({id, tittle, link}) => {
-                                return(
-                                    <li key={id}><a className='px-4 py-4 duration-300 hover:text-primary' href={link}>{tittle}</a></li>
-                                )
-                            })
-                        }
-                    </ul>
-                    <button className='px-4 py-1 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105'>Order</button>
-                </div>
-            </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+
+            <button className='px-4 py-1 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105 hidden md:flex'>
+              Order
+            </button>
+
+            {menu ? (
+              <IoMdClose
+                onClick={() => setMenu(false)}
+                className='text-4xl cursor-pointer md:hidden'
+              />
+            ) : (
+              <IoIosMenu
+                onClick={() => setMenu(true)}
+                className='text-4xl cursor-pointer md:hidden'
+              />
+            )}
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
