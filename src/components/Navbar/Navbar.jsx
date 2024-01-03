f285a98c6f7641e474a87b125e4c8388730dccab
Nelson Obuya Ouya <nelsonobuya19@gmail.com>
2024-01-04 00:36:04 +0300

    Made the Navbar well responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+// Navbar.js
+
 import { useState } from 'react';
 import Logo from '../../assets/logo.png';
 import { IoIosMenu, IoMdClose } from 'react-icons/io';
@@ -33,63 +35,60 @@ function Navbar() {
   };
 
   return (
-    <div className='shadow-xl'>
-      <div className='container md:py-3 sm:py-0 '>
-        <div className='flex items-center justify-between '>
-          <div className=''>
-            <a href='/#'>
-              <img src={Logo} alt='' className='w-16' />
-            </a>
-          </div>
-          <div className='flex items-center justify-between gap-4'>
-            <ul className='items-center hidden gap-4 md:flex'>
-              {NavLinks.map(({ id, title, link }) => (
-                <li key={id}>
-                  <a
-                    className='px-4 py-4 duration-300 hover:text-primary'
-                    href={link}
-                    onClick={handleLinkClick} // Close menu when link is clicked
-                  >
-                    {title}
-                  </a>
-                </li>
-              ))}
-            </ul>
+    <div className='fixed w-full bg-white shadow-lg z-10'>
+      <div className='container mx-auto py-3 px-4 md:px-0 flex items-center justify-between'>
+        <div>
+          <a href='/#'>
+            <img src={Logo} alt='' className='w-16' />
+          </a>
+        </div>
+        <div className='flex items-center justify-between gap-4'>
+          {/* Displaying links for screens above 768px */}
+          <ul className='hidden md:flex flex-grow items-center justify-center gap-4'>
+            {NavLinks.map(({ id, title, link }) => (
+              <li key={id}>
+                <a
+                  className='px-4 py-4 duration-300 hover:text-primary'
+                  href={link}
+                  onClick={handleLinkClick}
+                >
+                  {title}
+                </a>
+              </li>
+            ))}
+          </ul>
 
-            {menu && (
-              <ul className='items-center gap-4 flex nav-links md:hidden mx-auto'>
-                {NavLinks.map(({ id, title, link }) => (
-                  <li key={id}>
-                    <a
-                      className='px-4 py-4 duration-300 hover:text-primary'
-                      href={link}
-                      onClick={handleLinkClick} // Close menu when link is clicked
-                    >
-                      {title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            )}
-
-            <button className='px-4 py-1 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105 hidden md:flex'>
-              Order
-            </button>
-
-            {menu ? (
-              <IoMdClose
-                onClick={() => setMenu(false)}
-                className='text-4xl cursor-pointer md:hidden'
-              />
-            ) : (
-              <IoIosMenu
-                onClick={() => setMenu(true)}
-                className='text-4xl cursor-pointer md:hidden'
-              />
-            )}
-          </div>
+          {/* Hamburger menu icon for screens below 768px */}
+          {!menu ? (
+            <IoIosMenu
+              onClick={() => setMenu(true)}
+              className='text-4xl cursor-pointer md:hidden'
+            />
+          ) : (
+            <IoMdClose
+              onClick={() => setMenu(false)}
+              className='text-4xl cursor-pointer md:hidden'
+            />
+          )}
         </div>
       </div>
+
+      {/* Displaying links for screens below 768px */}
+      {menu && (
+        <ul className='flex flex-col md:hidden fixed top-16 w-full bg-white py-2 text-center'>
+          {NavLinks.map(({ id, title, link }) => (
+            <li key={id}>
+              <a
+                className='block px-4 py-2 duration-300 hover:text-primary'
+                href={link}
+                onClick={handleLinkClick}
+              >
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
